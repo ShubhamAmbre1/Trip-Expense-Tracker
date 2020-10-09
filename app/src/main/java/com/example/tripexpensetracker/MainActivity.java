4e34aaefae377f4bdf310387e691aa6932c523a1
ShubhamAmbre1 <shubhamambre94@gmail.com>
2020-10-10 03:24:16 +0530

    drawer

--- app/src/main/java/com/example/tripexpensetracker/MainActivity.java
+++ app/src/main/java/com/example/tripexpensetracker/MainActivity.java
@@ -1,41 +1,78 @@
 package com.example.tripexpensetracker;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.app.Notification;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Toolbar toolbar;
+    private TextView textViewUsername, textViewUserEmail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //Intent intent = new Intent(this, Login.class);
-        //startActivity(intent);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+        toolbar = findViewById(R.id.toolbar);
+
+        setSupportActionBar(toolbar);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        navigationView.setNavigationItemSelectedListener(this);
 
         if(!SharedPrefManager.getInstance(this).isLoggedIn()){
             finish();
             startActivity(new Intent(this, Login.class));
+        }
+
+        textViewUserEmail = findViewById(R.id.display_user_email);
+        textViewUsername = findViewById(R.id.display_user_name);
+
+        //textViewUserEmail.setText("abcd");
+        //textViewUsername.setText(SharedPrefManager.getInstance(this).getUsername());
+        View headerView = navigationView.getHeaderView(0);
+        textViewUsername = headerView.findViewById(R.id.display_user_name);
+        textViewUsername.setText(SharedPrefManager.getInstance(this).getUsername());
+
+        textViewUserEmail = headerView.findViewById(R.id.display_user_email);
+        textViewUserEmail.setText(SharedPrefManager.getInstance(this).getUserEmail());
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
         }
     }
 
     // To display options menu on the display
     // And also display the items for the menu
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu, menu);
-        return true;
-    }
-
-    //To check which option is selected
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         switch(item.getItemId()){
-            case R.id.menuLogout:
+            case R.id.logout:
                 SharedPrefManager.getInstance(this).logout();
                 finish();
                 startActivity(new Intent(this, Login.class));
